525b106ebca21d601019227b1ad9408daf70126a
mohAttallah <mohmohmoud28@gmail.com>
2023-09-04 22:44:15 +0300

    starter Code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Todo from './Components/Todo';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <>
+                <Todo />
+                
+            </>
+            
+        );
+    }
+}
