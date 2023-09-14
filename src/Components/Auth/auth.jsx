3af33665bf467a8cfae6c89a415874502fa9af71
mohAttallah <mohmohmoud28@gmail.com>
2023-09-14 20:27:06 +0300

    lab 34

--- src/Components/Auth/auth.jsx
+++ src/Components/Auth/auth.jsx
@@ -6,14 +6,8 @@ import { LoginContext } from './context';
 function Auth(props) {
     const context = useContext(LoginContext);
     const isLoggedIn = context.loggedIn;
-    //check the capability
     const canDo = props.capability ? context.can(props.capability) : true;
     const okToRender = isLoggedIn && canDo;
-
-    console.log("isLoggedIn:", isLoggedIn);
-    console.log("capability:", props.capability);
-    console.log("canDo:", canDo);
-    console.log("okToRender:", okToRender);
 
     return (
         <When condition={okToRender}>
